b44b140a2c3197e9471a0f1cc14d05b7e25c8c1b
Rogier Verkaik <rogierv@gmail.com>
2019-04-08 10:58:08 +0200

    Testing Expenses Selector

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -6,8 +6,7 @@ export default (expenses, { text, sortBy, startDate, endDate }) => {
       const createdAtMoment = moment(expense.createdAt);
       const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
       const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
-      const textMatch = typeof text !== 'string' || expense.description.toLowerCase().includes(text.toLowerCase());
-
+      const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
       return startDateMatch && endDateMatch && textMatch;
     })
     .sort((a, b) => {
