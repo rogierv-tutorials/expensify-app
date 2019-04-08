78661d4106a325411c019da5d9d2ffbc466a0e96
Rogier Verkaik <rogierv@gmail.com>
2019-04-08 09:12:31 +0200

    Testing Filters Action Generators

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,40 @@
+import moment from 'moment';
+import {
+  setStartDate,
+  setEndDate,
+  setTextFilter,
+  sortByAmount,
+  sortByDate
+} from '../../actions/filters';
+
+test('should generate set start date action object', () => {
+  const action = setStartDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_START_DATE',
+    startDate: moment(0)
+  });
+});
+
+test('should generate set end date action object', () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_END_DATE',
+    endDate: moment(0)
+  });
+});
+
+test('should generate set text filter action object', () => {
+  expect(setTextFilter('rent')).toEqual({ type: 'SET_TEXT_FILTER', text: 'rent' });
+});
+
+test('should generate default set text filter action object', () => {
+  expect(setTextFilter()).toEqual({ type: 'SET_TEXT_FILTER', text: '' });
+});
+
+test('should generate sort by amount action object', () => {
+  expect(sortByAmount()).toEqual({ type: 'SORT_BY_AMOUNT' });
+});
+
+test('should generate sort by date action object', () => {
+  expect(sortByDate()).toEqual({ type: 'SORT_BY_DATE' });
+});
