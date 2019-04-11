6d5fed2eb881e2d4267d3c1f9c78c5f71d7e62c4
Rogier Verkaik <rogierv@gmail.com>
2019-04-11 10:31:32 +0200

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.get('*', (_, res) => res.sendFile(path.join(publicPath, 'index.html')));
 
-app.listen(3000, () => console.log('Server is up'));
+app.listen(port, () => console.log('Server is up'));
